955c5f55b1bbd606317e080db045c4f0ec52191d
吴家荣 <973430584@qq.com>
2021-03-01 18:20:38 +0800

    更新首页样式

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,10 +10,18 @@ div
       :style="`background-color: hsl(180, ${$route.path == '/color' ? '50%' : '100%'}, 40%); text-decoration: none`"
     ) 色相轮
   Nuxt
+  .h-150(style="background-color: hsl(0, 0%, 95%)")
+    .h-98.lh-98.t-c.fs-36 雄关漫道真如铁，而今迈步从头越
+    .h-12.lh-12.fs-12.t-r.pr-380(style="color: #999") —— 毛泽东·《忆秦娥·娄山关》
+    .h-40.lh-40.fs-14.t-c(style="color: #aaa") Copyright @ wujr5.cn, {{year}}
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      year: new Date().getFullYear()
+    }
+  }
 }
 </script>
